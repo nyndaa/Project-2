672b74f3f075b13842c4db726200353db9ff9381
nyndaa <raharjiansustanadiawijayanti@gmail.com>
2026-05-29 20:24:26 +0700

    Sesi 3

--- app/(landing)/components/payment/payment-options.tsx
+++ app/(landing)/components/payment/payment-options.tsx
@@ -1,44 +1,35 @@
-import { FiCreditCard } from "react-icons/fi";
-import CardWithHeader from "../ui/card-with-header";
+"use client";
 
-const paymentList = [
-  {
-    bank_name: "BCA",
-    account_number: 1231231231231,
-    account_holder: "PT SportsOn Digital",
-  },
-  {
-    bank_name: "Mandiri",
-    account_number: 89458434,
-    account_holder: "PT SportsOn Digital",
-  },
-  {
-    bank_name: "BRI",
-    account_number: 123891283912,
-    account_holder: "PT SportsOn Digital",
-  },
-];
+import Image from "next/image";
+import Button from "../ui/button";
+import { FiRefreshCw } from "react-icons/fi";
 
-const PaymentOptions = () => {
+const OrderSubmitted = () => {
+  const reloadOrderStatus = () => {
+    window.location.reload();
+  };
+
   return (
-    <CardWithHeader title="Payment Options">
-      {paymentList.map((payment, index) => (
-        <div className="flex gap-5 p-5 border-b border-gray-100" key={index}>
-          <div className="bg-blue-100 p-4 text-blue-500 h-fit self-center">
-            <FiCreditCard size={24} />
-          </div>
-          <div className="self-center">
-            <div className="font-bold">{payment.bank_name}</div>
-            <div className="text-sm">{payment.account_number}</div>
-            <div className="text-sm opacity-70">{payment.account_holder}</div>
-          </div>
-          <div className="ml-auto bg-blue-50 text-gray-800 text-xs h-fit self-center px-2 py-1">
-            Bank Transfer
-          </div>
-        </div>
-      ))}
-    </CardWithHeader>
+    <div className="bg-white w-160 p-16 flex flex-col justify-center items-center mx-auto">
+      <Image
+        src="/images/icon-order-submitted.svg"
+        width={117}
+        height={117}
+        alt="order submitted"
+        className="mb-4"
+      />
+      <h2 className="text-2xl font-semibold mb-2">Order Submitted!!</h2>
+      <p className="text-center mb-8">
+        Your Order is recorded in our system, we are still confirming the
+        payment status, please wait and your order status will be updated in
+        less than 12 hours.
+      </p>
+      <Button variant="dark" className="w-full" onClick={reloadOrderStatus}>
+        <FiRefreshCw />
+        Refresh Order Status
+      </Button>
+    </div>
   );
 };
 
-export default PaymentOptions;
+export default OrderSubmitted;
--- app/(landing)/components/ui/button.tsx
+++ app/(landing)/components/ui/button.tsx
@@ -1,39 +1,67 @@
-type TButtonProps = {
-  children: React.ReactNode;
-  className?: string;
-  variant?: "primary" | "dark" | "ghost";
-  size?: "normal" | "small";
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+"use client";
 
-const Button = ({
-  children,
-  className,
-  variant = "primary",
-  size = "normal",
-  ...props
-}: TButtonProps) => {
-  const baseStyles =
-    "inline-flex gap-2 duration-300 justify-center items-center cursor-pointer hover:scale-105";
+import {
+  FiArrowRight,
+  FiChevronDown,
+  FiChevronUp,
+  FiShoppingBag,
+} from "react-icons/fi";
+import Button from "../ui/button";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useCartStore } from "@/app/hooks/use-cart-store";
+import { Product } from "@/app/types";
 
-  const variants = {
-    primary: "bg-primary text-white hover:bg-primary/85",
-    dark: "bg-dark text-white hover:bg-dark/85",
-    ghost: "bg-transparent hover:bg-gray-100 text-dark",
+type TProductActionsProps = {
+  product: Product;
+  stock: number;
+};
+
+const ProductActions = ({ product, stock }: TProductActionsProps) => {
+  const { addItem } = useCartStore();
+  const { push } = useRouter();
+  const [qty, setQty] = useState(1);
+
+  const handleAddToCart = () => {
+    addItem(product, qty);
   };
 
-  const sizes = {
-    normal: "py-4 px-9",
-    small: "py-[10px] px-7",
+  const handleCheckout = () => {
+    addItem(product);
+    push("/checkout");
   };
 
   return (
-    <button
-      className={`${baseStyles} ${variants[variant]} ${sizes[size]} ${className}`}
-      {...props}
-    >
-      {children}
-    </button>
+    <div className="flex gap-5">
+      <div className="border border-gray-500 inline-flex w-fit min-w-20.5">
+        <div className="aspect-square text-xl font-medium border-r border-gray-500 flex justify-center items-center">
+          <span>{qty}</span>
+        </div>
+        <div className="flex flex-col">
+          <button
+            className="border-b border-gray-500 cursor-pointer h-1/2 aspect-square flex items-center justify-center"
+            onClick={() => setQty(qty < stock ? qty + 1 : qty)}
+          >
+            <FiChevronUp />
+          </button>
+          <button
+            className="cursor-pointer h-1/2 aspect-square flex items-center justify-center"
+            onClick={() => setQty(qty > 1 ? qty - 1 : qty)}
+          >
+            <FiChevronDown />
+          </button>
+        </div>
+      </div>
+      <Button className="px-20 w-full" onClick={handleAddToCart}>
+        <FiShoppingBag size={24} />
+        Add to Cart
+      </Button>
+      <Button variant="dark" className="px-20 w-full" onClick={handleCheckout}>
+        Checkout Now
+        <FiArrowRight size={24} />
+      </Button>
+    </div>
   );
 };
 
-export default Button;
+export default ProductActions;
